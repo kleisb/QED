c1b2cbb6f44331669a32648364dde68e60e8d719
Hector Rovira <hrovira@systemsbiology.org>
2013-03-15 15:48:50 -0700

    finished layout

--- app/views/admix_ratios.js
+++ app/views/admix_ratios.js
@@ -1,7 +1,9 @@
 var Template = require("./templates/admix_ratios");
 
 module.exports = Backbone.View.extend({
-    races: ["AMR", "AFA", "EUR", "ASN"],
+    bar_height: 10,
+    bar_gap: 3,
+    group_width: 100,
 
     initialize: function (options) {
         _.extend(this, options);
@@ -16,6 +18,11 @@ module.exports = Backbone.View.extend({
     },
 
     renderView: function () {
+        if (!this.options.groupings) {
+            console.log("no grouping specified");
+            return;
+        }
+
         var finderFn = function (item) {
             return _.contains(_.values(item), this.options.subpopulation);
         };
@@ -23,55 +30,50 @@ module.exports = Backbone.View.extend({
         var subpopulation = _.find(this.model.get("items"), finderFn, this) || {};
         var data = _.reject(this.model.get("items"), finderFn, this) || [];
 
-        var m = [20, 40, 0, 40];
-        var w = 300;
-        var h = 50;
+        var bar_height = this.options.bar_height || this.bar_height;
+        var bar_gap = this.options.bar_gap || this.bar_gap;
+        var group_width = this.options.group_width || this.group_width;
+        var full_height = bar_height * 3 + (data.length * (bar_height + bar_gap));
 
-        var strokeFn = d3.scale.category20b();
+        var svg = d3.select(_.first(this.$el.find(".c-admix-ratios"))).append("svg")
+            .attr("width", this.options.groupings.length * group_width)
+            .attr("height", full_height);
 
-        var axis = d3.svg.axis().ticks(0).orient("left");
-        var x = d3.scale.ordinal().domain(this.races).rangePoints([0, w]);
-        var y = {};
-        _.each(this.races, function (d) {
-            y[d] = d3.scale.linear().domain([0, 1]).range([h, 0]);
+        var g_container = svg.append("g").attr("class", "admix-columns").attr("transform", "translate(4,4)");
+
+        _.each(this.options.groupings, function (grouping, idx) {
+            var data_points = _.map(data, function (item) {
+                return parseFloat(item[grouping]);
+            }, this);
+
+            var width = d3.scale.linear().domain([0, 1]).range([0, (group_width * 0.9)]);
+            var position = d3.scale.ordinal().domain(_.range(data_points.length)).rangeBands([0, data_points.length * (bar_height + bar_gap)]);
+
+            var rect_group = g_container.append("g").attr("transform", "translate(" + idx * group_width + ",0)");
+
+            rect_group.append("text")
+                .attr("y", bar_height)
+                .text(function () {
+                    if (_.has(subpopulation, grouping)) return grouping + " (" + subpopulation[grouping] + "%)";
+                    return grouping;
+                });
+
+            rect_group.append("line")
+                .attr("x1", 0)
+                .attr("x2", 0)
+                .attr("y1", bar_height * 1.5)
+                .attr("y2", full_height);
+
+            rect_group.selectAll("rect")
+                .data(data_points)
+                .enter()
+                .append("rect")
+                .attr("y", function (d, i) {
+                    return (bar_height * 2) + position(i);
+                })
+                .attr("width", width)
+                .attr("height", bar_height);
+
         });
-
-        var _this = this;
-        var line = d3.svg.line();
-        var path = function (d) {
-            return line(_this.races.map(function (p) {
-                return [x(p), y[p](d[p])];
-            }));
-        };
-
-        var svg = d3.select(_.first(this.$el.find(".c-admix-ratios"))).append("svg:svg")
-            .attr("width", w + m[1] + m[3])
-            .attr("height", h + m[0] + m[2])
-            .append("svg:g").attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-        // Add foreground lines.
-        var idx = 0;
-        svg.append("svg:g").attr("class", "foreground").selectAll("path").data(data)
-            .enter().append("svg:path").attr("d", path).attr("stroke", function (d) {
-                return strokeFn(idx++ % 20);
-            });
-
-        // Add a group element for each trait.
-        var g = svg.selectAll(".trait").data(this.races).enter().append("svg:g").attr("class", "trait")
-            .attr("transform", function (d) {
-                return "translate(" + x(d) + ")";
-            });
-
-        // Add an axis and title.
-        g.append("svg:g").attr("class", "axis")
-            .each(function (d) {
-                d3.select(this).call(axis.scale(y[d]));
-            })
-            .append("svg:text").attr("text-anchor", "middle").attr("y", -9)
-            .text(function (d) {
-                var subpop = subpopulation[d];
-                if (subpop) return d + " (" + subpop + "%)";
-                return d;
-            });
     }
 });
